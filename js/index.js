67d1593c9c57469c185b1a618183329881637237
A <adcollins91@gmail.com>
2016-04-13 12:46:19 -0400

    V-1-2-2
    index.js
    1) Fix issue with multiple AJAX calls by using ‘callback’ing the
    populateTabs.
    2) Alphabetize the usprlist with mapSort
    (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Globa
    l_Objects/Array/sort). Sorting with the use of map() is one way to
    implement case-insensitive sorting.
    
    index.html
    1) Change titles of each .tab-button

--- js/index.js
+++ js/index.js
@@ -1,22 +1,61 @@
 var userList = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
 
-function populateTabs(list) {
+function mapSort(list) {
+    // temporary array holds objects with position and sort-value
+    var mapped = list.map(function(el, i) {
+      return { index: i, value: el.toLowerCase() };
+    })
+
+    // sorting the mapped array containing the reduced values
+    mapped.sort(function(a, b) {
+      return +(a.value > b.value) || +(a.value === b.value) - 1;
+    });
+
+    // container for the resulting order
+    var result = mapped.map(function(el){
+      return list[el.index];
+    });
+    
+    return result;
+}
+
+function populateTabs(list, index) {
     var url;
     var username;
     var on_offline = "Offline";
     var stringToAppend;
-    for (var i = 0; i < list.length; i++) {
-        username = list[i];
-        url = 'https://api.twitch.tv/kraken/streams/'+username+'?callback=?';
-        $.getJSON(url, function (data) {
-            if (data.Stream != null) {
-                    on_offline = "Online";
-                }
-                stringToAppend = "<div class = 'test-div'><p>Username: "+username+"</p><p>"+on_offline+"</p></div>"
-                $('.tab-1').append(stringToAppend);
-            }
-        );
+    
+    // Alphabetize list
+    // IF index is not provided, then this is the first time populateTabs() has been called...
+    if (!index) {
+        // Sort list...
+        list = mapSort(list);
+        // Set index to 0
+        index = 0;
     }
+    // Set username and url, using index
+    username = list[index];
+    url = 'https://api.twitch.tv/kraken/streams/'+username+'?callback=?';
+    // AJAX call to Twitch API
+    $.getJSON(url, function (data) {
+        if (data.Stream != null) {
+                on_offline = "Online";
+            }
+            stringToAppend = "<div class = 'test-div'>Username: "+username+"<br>"+on_offline+"</div>"
+            $('.tab-1').append(stringToAppend);
+            
+            // If there are more usernames in the 'list'...
+            if (index < list.length - 1) {
+                // Increment index...
+                index += 1;
+                // And call populateTabs() again
+                populateTabs(list, index);
+            } else {
+                // Else you're at the end of the list...
+                // Do something (if you'd like)...
+            }
+        }
+    );
 }
 
 $(document).ready(function () {
